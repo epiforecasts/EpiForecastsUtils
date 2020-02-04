c1f94d1422cb7b1dfc45e2858378d8ad5452a757
JDMunday <james.munday@lshtm.ac.uk>
2020-02-04 10:12:40 +0000

    make functions work

--- R/weibull_with_mean_sd.R
+++ R/weibull_with_mean_sd.R
@@ -5,27 +5,27 @@
 #' @export
 #'
 #' @examples
-#' 
-#' ## Example 
+#'
+#' ## Example
 #' sample <- rweibull_with_mean_sd(1000, 3, 5)
-#' 
+#'
 #' mean(sample)
-#' 
+#'
 #' sd(sample)
-#' 
-#' 
+#'
+#'
 #' hist(sample)
-#' ## Code 
+#' ## Code
 #' rweibull_with_mean_sd
 rweibull_with_mean_sd <- function(n, mean, sd) {
-  
+
   mu <- mean
   sigma <- sd
   loc <- 0
-  
+
   ## Code adapted from the mixdist package
   cv <- sigma/(mu - loc)
-  
+
   if (cv < 1e-06) {
     nu <- cv/(sqrt(trigamma(1)) - cv * digamma(1))
     shape <- 1/nu
@@ -35,17 +35,17 @@ rweibull_with_mean_sd <- function(n, mean, sd) {
     aa <- log(cv^2 + 1)
     nu <- 2 * cv/(1 + cv)
     repeat {
-      gb <- (lgamma(1 + 2 * nu) - 2 * 
+      gb <- (lgamma(1 + 2 * nu) - 2 *
                lgamma(1 + nu) - aa) /
         (2 * (digamma(1 + 2 * nu) - digamma(1 + nu)))
       nu <- nu - gb
-      if (abs(gb) < 1e-12) 
+      if (abs(gb) < 1e-12)
         break
     }
     shape <- 1/nu
     scale <- exp(log(mu - loc) - lgamma(1 + nu))
   }
-  
+
   stats::rweibull(n, shape = shape, scale = scale)
 }
 
@@ -56,27 +56,16 @@ rweibull_with_mean_sd <- function(n, mean, sd) {
 #' @export
 #'
 #' @examples
-#' 
-#' ## Example 
-#' sample <- rweibull_with_mean_sd(1000, 3, 5)
-#' 
-#' mean(sample)
-#' 
-#' sd(sample)
-#' 
-#' 
-#' hist(sample)
-#' ## Code 
-#' rweibull_with_mean_sd
-pweibull_with_mean_sd <- function(n, mean, sd) {
-  
+#'
+pweibull_with_mean_sd <- function(q, mean, sd) {
+
   mu <- mean
   sigma <- sd
   loc <- 0
-  
+
   ## Code adapted from the mixdist package
   cv <- sigma/(mu - loc)
-  
+
   if (cv < 1e-06) {
     nu <- cv/(sqrt(trigamma(1)) - cv * digamma(1))
     shape <- 1/nu
@@ -86,16 +75,16 @@ pweibull_with_mean_sd <- function(n, mean, sd) {
     aa <- log(cv^2 + 1)
     nu <- 2 * cv/(1 + cv)
     repeat {
-      gb <- (lgamma(1 + 2 * nu) - 2 * 
+      gb <- (lgamma(1 + 2 * nu) - 2 *
                lgamma(1 + nu) - aa) /
         (2 * (digamma(1 + 2 * nu) - digamma(1 + nu)))
       nu <- nu - gb
-      if (abs(gb) < 1e-12) 
+      if (abs(gb) < 1e-12)
         break
     }
     shape <- 1/nu
     scale <- exp(log(mu - loc) - lgamma(1 + nu))
   }
-  
-  stats::pweibull(n, shape = shape, scale = scale)
+
+  stats::pweibull(q, shape = shape, scale = scale)
 }
